b68d43a98a8f4d57db64b5cd041f81f100ee502a
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2025-06-23 05:56:25 +0800

    icon logo

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+// src/assets/index.js
+
+import uig from './company/uig.png';
+import atmajaya from './company/atmajaya.png';
+import bangkitacademy from './company/bangkitacademy.png';
+import dbs from './company/dbs.png';
+
+export {
+  uig,
+  atmajaya,
+  bangkitacademy,
+  dbs,
+};
