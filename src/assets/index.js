cd5bf2bb14b13bfcfc008704619b3aa0c0a9257c
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2025-06-25 18:45:52 +0800

    certificates img

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,39 @@
-// src/assets/index.js
-
+// company
 import uig from './company/uig.png';
 import atmajaya from './company/atmajaya.png';
 import bangkitacademy from './company/bangkitacademy.png';
 import dbs from './company/dbs.png';
 
+// certificate
+import bangkit2024 from './certificates/bangkit2024.png';
+import mbkmuaj from './certificates/mbkmuaj.png';
+import dicodingBBAGC from './certificates/dicodingBBAGC.png';
+import dicodingBDPJS from './certificates/dicodingBDPJS.png';
+import dicodingBDPW from './certificates/dicodingBDPW.png';
+import dicodingBFFWD from './certificates/dicodingBFFWD.png';
+import dicodingBPJS from './certificates/dicodingBPJS.png';
+import dicodingDAI from './certificates/dicodingDAI.png';
+import dicodingGCE from './certificates/dicodingGCE.png';
+import dicodingGit from './certificates/dicodingGit.png';
+import dicodingMLGC from './certificates/dicodingMLGC.png';
+import dicodingPWI from './certificates/dicodingPWI.png';
+
+
 export {
+  // certificate
+  bangkit2024,
+  mbkmuaj,
+  dicodingBBAGC,
+  dicodingBDPJS,
+  dicodingBDPW,
+  dicodingBFFWD,
+  dicodingBPJS,
+  dicodingDAI,
+  dicodingGCE,
+  dicodingGit,
+  dicodingMLGC,
+  dicodingPWI,
+  // company
   uig,
   atmajaya,
   bangkitacademy,
